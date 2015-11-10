24e588825e6e036411da437c15dc7f7d835de5e4
Camilla Willim <cwillim@pc02.pool.tp1.uni-duesseldorf.de>
2015-11-10 16:53:45 +0100

    endlich *phew*

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,52 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+
+using namespace std;
+
+ complex <double> f(double p,complex <double> ii);
 
 int main(){
-
+  int N=100;
+ 
   // Example how to declare a single complex variable:
-  complex<double> c;
+  complex<double> c[N];
+  complex <double> z[N];
+ 
+  double phi[N];
+  phi[0]=0.0;
 
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  // define i
+  complex<double> ii = complex<double>(0.0, 1.0); 
+ 
+  
+  double dphi = (M_PI*2)/(N-1);
+  
+  //for(double phi=0.0; phi<=M_PI*2;phi+=dphi){
+  
+  for(int i=0;i<N;i++){
+    
+    phi[i]=i*dphi;
+    c[i]=exp(phi[i]*ii);
+    z[i] = f(phi[i],ii);
+    
+  }
+  
+  for(int j=0;j<N;j++){
+    cout << phi[j] << "\t" << real(c[j])<< "\t"<<imag(c[j])<< "\t"<< abs(c[j])<< "\t" << z[j]<< endl;
+    
+    
+  }
+  
+  
 
   return 0;
+}
+
+complex <double> f(double p,complex <double> ii){
+ 
+ 
+ 
+   return tan(p*(1.0+ii));
+  
 }
